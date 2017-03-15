0478828c6ee9b61aee5f4b31f51c21aa91e55f4e
djstojadin <djstojadin@gmail.com>
2017-03-15 22:54:06 +0100

    Style about page and link to recourses

--- app/components/About.jsx
+++ app/components/About.jsx
@@ -12,8 +12,19 @@ var React = require('react');
 var About = (props) => {
   return (
     <div>
-      <h5>About page</h5>
-      <p>Welcome to the About page!!!</p>
+      <h1 className="text-center">About page</h1>
+      <p>This is a weather application build on React. I have built this for Complete React Web App Developer Course.</p>
+      <p>
+        Tools I used for this course:
+      </p>
+      <ul>
+        <li>
+          <a href="https://facebook.github.io/react">React</a> - JavaScript Framework
+        </li>
+        <li>
+          <a href="http://openweathermap.org">Open Weather Map</a> - API
+        </li>
+      </ul>
     </div>
   );
 };
