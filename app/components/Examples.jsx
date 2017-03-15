d0810f01ab7051b04e0a916350b98fd8a8188c61
djstojadin <djstojadin@gmail.com>
2017-03-15 21:52:11 +0100

    Rename example page

--- app/components/Examples.jsx
+++ app/components/Examples.jsx
@@ -13,7 +13,7 @@ var React = require('react');
 var Examples = (props) => {
   return (
     <div>
-      <h5>Examples component!</h5>
+      <h3>Examples</h3>
       <p>Welcome to examples page!</p>
     </div>
   );
