f4ae53c37d7308cf016085d407aebc5c42b769e1
djstojadin <djstojadin@gmail.com>
2017-03-16 19:59:18 +0100

    Add error modal copy

--- app/api/openWeatherMap.jsx
+++ app/api/openWeatherMap.jsx
@@ -1,12 +1,6 @@
 var axios = require('axios');
 
-// const OPEN_WEATHER_MAP_URL = 'http://samples.openweathermap.org/data/2.5/weather?appid=fe04bab564a2952d9b620514a90e4fb4';
-
 const OPEN_WEATHER_MAP_URL = 'http://api.openweathermap.org/data/2.5/weather?appid=fe04bab564a2952d9b620514a90e4fb4&units=metric';
-
-// http://samples.openweathermap.org/data/2.5/weather?q=London,uk&appid=fe04bab564a2952d9b620514a90e4fb4
-
-
 
 module.exports = {
   getTemp: function (location) {
@@ -19,8 +13,8 @@ module.exports = {
       } else {
         return res.data.main.temp;
       }
-    }, function (res) {
-      throw new Error(res.data.message);
+    }, function (err) {
+      throw new Error('Unable to fetch weather for that location.');
     });
   }
 }
--- app/components/ErrorModal.jsx
+++ app/components/ErrorModal.jsx
@@ -1,18 +1,30 @@
 var React = require('react');
 
 var ErrorModal = React.createClass({
+  getDefaultProps: function () {
+    return {
+      title: 'Error'
+    };
+  },
+
+  propTypes: {
+    title: React.PropTypes.string,
+    message: React.PropTypes.string.isRequired
+  },
+
   componentDidMount: function () {
     var modal = new Foundation.Reveal($('#error-modal'));
-    modal.open();  
+    modal.open();
   },
 
   render: function () {
+    var {title, message} = this.props;
     return (
-      <div id="error-modal" className="reveal tiny text-center" data-reveal>
-        <h4>Some title</h4>
-        <p>Error message!</p>
+      <div id="error-modal" className="reveal tiny text-center" data-reveal="">
+        <h4>{title}</h4>
+        <p>{message}</p>
         <p>
-          <button className="close-button" data-close aria-label="Close modal" type="button"></button>
+          <button className="button hollow" data-close="">Okay</button>
         </p>
       </div>
     );
